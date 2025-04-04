d4ad0215da2d89d82822d8d4f19b50936bfd1a52
hieupth <64821726+hieupth@users.noreply.github.com>
2025-04-04 20:00:30 +0700

    chore: add ansible-vault encryption

--- README.md
+++ README.md
@@ -1,5 +1,16 @@
 # Certbot image
 Simple certbot image with Cloudflare DNS plugin enabled for my personal work.
+## Usage
+Place your Cloudflare DNS token at `cloudflare.ini` file and then run command:
+```bash
+docker run --rm \
+  -v ./letsencrypt:/etc/letsencrypt \
+  -v ./cloudflare.ini:/cloudflare.ini \
+  -e DOMAINS=*.example.com,example.com \
+  -e EMAIL=me@example.com \
+  -e CLOUDFLARE_TOKEN_FILE=/cloudflare.ini \
+  hieupth/certbot
+```
 ## License
 [Apache License 2.0](LICENSE).<br>
 Copyright &copy; 2023 [Hieu Pham](https://github.com/hieupth). All rights reserved.
--- entrypoint.sh
+++ entrypoint.sh
@@ -8,28 +8,41 @@ get_certificate() {
     --email $EMAIL -d $domain $args
   # ec=$?
   # echo "certbot exit code $ec"
+  local d=$(echo $d | sed 's/^*.//')
+  # Concat the full chain with the private key (e.g. for haproxy)
+  cat /etc/letsencrypt/live/$d/fullchain.pem /etc/letsencrypt/live/$d/privkey.pem > /etc/letsencrypt/live/$d/concat.pem
+  # Keep full chain and private key in separate files (e.g. for nginx and apache)
+  mkdir -p /certs/$d/ && cp /etc/letsencrypt/live/$d/fullchain.pem /certs/$d/fullchain.pem
+  mkdir -p /certs/$d/ && cp /etc/letsencrypt/live/$d/privkey.pem /certs/$d/privkey.pem
+  mkdir -p /certs/$d/ && cp /etc/letsencrypt/live/$d/cert.pem /certs/$d/cert.pem
+  mkdir -p /certs/$d/ && cp /etc/letsencrypt/live/$d/chain.pem /certs/$d/chain.pem
+  mkdir -p /certs/$d/ && cp /etc/letsencrypt/live/$d/concat.pem /certs/$d/concat.pem
+  # Encrypt all certs.
+  if ! [[ -z "$ENCRYPT_PASS_FILE" ]]; then
+    ansible-vault encrypt --vault-password-file $ENCRYPT_PASS_FILE \
+      /certs/$d/fullchain.pem \
+      /certs/$d/privkey.pem \
+      /certs/$d/cert.pem \
+      /certs/$d/chain.pem \
+      /certs/$d/concat.pem
+  fi
 }
 
 # Build arguments for certbot command.
 args=""
-# Select DNS plugins.
-if ! [[ -z $WEBROOT ]]; then
-  echo "Using webroot: $WEBROOT"
-  args="--webroot -w $WEBROOT"
-elif ! [[ -z $CLOUDFLARE_TOKEN_FILE ]]; then
-  echo "Using Cloudflare DNS..."
-  args="--dns-cloudflare --dns-cloudflare-credentials $CLOUDFLARE_TOKEN_FILE --dns-cloudflare-propagation-seconds $PROPAGATION"
-# Use standalone if none of them is selected.
+# Debug only,
+if [ "$DEBUG" = true ]; then
+  echo "Debug mode...";
+  args=$args" --debug --dry-run";
+# Obtain with plugins.
 else
-  echo "Using standalone..."
-  args="--standalone --preferred-challenges http-01"
-fi
-
-# Enable debug (dry-run) mode.
-if [ "$DEBUG" = true ]
-then
-  echo "Debug mode..."
-  args=$args" --debug --dry-run"
+  if ! [[ -z $CLOUDFLARE_TOKEN_FILE ]]; then
+    args="--dns-cloudflare --dns-cloudflare-credentials $CLOUDFLARE_TOKEN_FILE --dns-cloudflare-propagation-seconds $PROPAGATION"
+  elif ! [[ -z $WEBROOT ]]; then
+    args="--webroot -w $WEBROOT"
+  else
+    args="--standalone --preferred-challenges http-01"
+  fi
 fi
 
 # Seperate domains and obtain certs.
